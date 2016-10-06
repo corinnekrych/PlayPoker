cf147069519950400219a3524ef170a31bcb973f
Corinne Krych <corinnekrych@gmail.com>
2016-10-06 17:26:33 +0100

    hamburger template10 scaffolding

---
+++ PlayPoker/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using PlayPoker.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace PlayPoker.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ PlayPoker/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using PlayPoker.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace PlayPoker.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
